8dc537eea1adf3b148f5f6933f402d62f4c3d4fe
seungjun-green <lsj3285007@gmail.com>
2024-02-12 23:18:24 +0900

    Initial Commit

---
+++ SW/SWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWApp.swift
+//  SW
+//
+//  Created by SeungJun Lee on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
